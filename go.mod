29f23a405dfd631de9eb7273f648d42d37ed83a7
xzw <3088655042@qq.com>
2026-03-01 23:35:05 +0800

    feat:task exec operation

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.25.5
 
 require (
 	github.com/fsnotify/fsnotify v1.9.0
+	github.com/jessevdk/go-flags v1.6.1
 	github.com/natefinch/lumberjack v2.0.0+incompatible
+	github.com/robfig/cron/v3 v3.0.1
 	github.com/spf13/viper v1.21.0
 	go.etcd.io/etcd/client/v3 v3.6.8
 	go.uber.org/zap v1.27.1
@@ -23,7 +25,6 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 // indirect
-	github.com/jessevdk/go-flags v1.6.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
--- node/server/handler/task.go
+++ node/server/handler/task.go
@@ -4,10 +4,17 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
 
+	"github.com/robfig/cron/v3"
 	"github.com/xzwsloser/TaskGo/model"
 	"github.com/xzwsloser/TaskGo/pkg/etcdclient"
 	"github.com/xzwsloser/TaskGo/pkg/logger"
+	"github.com/xzwsloser/TaskGo/pkg/notify"
+	"github.com/xzwsloser/TaskGo/pkg/utils"
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
@@ -77,6 +84,234 @@ func GetTasks(nodeUUID string) (tasks Tasks, err error) {
 	}
 
 	return 
+}
+
+func WatchTasks(nodeUUID string) clientv3.WatchChan {
+	return etcdclient.GetEtcdClient().Watch(
+		fmt.Sprintf(etcdclient.KeyEtcdTaskPrefix, nodeUUID), clientv3.WithPrefix())
+}
+
+func GetTaskFromKey(key string) int {
+	idx := strings.LastIndex(key, "/")
+	if idx < 0 {
+		return 0
+	}
+
+	taskID, err := strconv.Atoi(key[idx+1:])
+	if err != nil {
+		return 0
+	}
+
+	return taskID
+}
+
+// @Description: Insert Task Log (Task Info) Into DB
+func (task *Task) CreateTaskLog() (int, error) {
+	start := time.Now()
+	taskLog := &model.TaskLog{
+		Name: task.Name,
+		TaskId: task.ID,
+		Command: task.Command,
+		IP: task.Ip,
+		Hostname: task.Hostname,
+		NodeUUID: task.RunOn,
+		Spec: task.Spec,
+		StartTime: start.Unix(),
+	}
+
+	return taskLog.Insert()
+}
+
+// @Description: Update After The Task Finished
+func (task *Task) UpdateTaskLog(taskLogId int, 
+								start time.Time, 
+								output string, 
+								retry int, 
+								success bool) error {
+	end := time.Now()
+	taskLog := &model.TaskLog{
+		ID: taskLogId,
+		StartTime: start.Unix(),
+		RetryTimes: retry,
+		Success: success,
+		Output: output,
+		EndTime: end.Unix(),
+	}
+
+	return taskLog.Update()
+}
+
+func (task *Task) Success(taskLogId int, 
+						  start time.Time, 
+						  output string, 
+						  retry int) error {
+	return task.UpdateTaskLog(taskLogId, start, output, retry, true)
+}
+
+func (task *Task) Fail(taskLogId int, 
+					   start time.Time, 
+					   output string, 
+					   retry int) error {
+	return task.UpdateTaskLog(taskLogId, start, output, retry, false)
+}
+
+// @Description: Run Task And Recover
+func (task *Task) RunWithRecover() {
+	// recover
+	defer func() {
+		if r := recover() ; r != nil {
+			const stackSize = 64 << 10 // 64 KB
+			buf := make([]byte, stackSize)
+			buf = buf[:runtime.Stack(buf, false)]
+			logger.GetLogger().Warn(fmt.Sprintf("panic running task: %v\n%s", r, buf))
+		}
+	}()
+
+	t := time.Now()
+	taskLogId, err := task.CreateTaskLog()
+	if err != nil {
+		logger.GetLogger().Warn(fmt.Sprintf("Failed to write task log: %v\n", err))
+	}
+	h := NewHandler(task)
+	if h == nil {
+		logger.GetLogger().Error(fmt.Sprintf("Task: %d Has No Type!", task.ID))
+		return 
+	}
+
+	result, runErr := h.Run(task)
+	if runErr != nil {
+		// Task Failed -> Notify User
+		err = task.Fail(taskLogId, t, runErr.Error(), 0)
+		if err != nil {
+			logger.GetLogger().Warn(fmt.Sprintf("Failed to write task log: %v\n", err))
+		}
+
+		node := &model.Node{UUID: task.RunOn}
+		err = node.FindByUUID()
+		if err != nil {
+			logger.GetLogger().Warn(fmt.Sprintf("Failed to find node: %v\n", err))
+		}
+
+		var notifiedUsers []string
+		for _, userId := range task.NotifyToArray {
+			userModel := &model.User{
+				ID: userId,
+			}
+			err = userModel.FindById()
+			if err != nil {
+				logger.GetLogger().Warn(fmt.Sprintf("Failed to Find User: %d\n", userId))
+				continue
+			}
+
+			notifiedUsers = append(notifiedUsers, userModel.Email)
+		}
+
+		msg := &notify.Message{
+			Type: task.NotifyType,
+			IP: fmt.Sprintf("%s:%s", node.IP, node.PID),
+			Subject: fmt.Sprintf("任务 [%s] 执行失败", task.Name),
+			Body: fmt.Sprintf("Task [%d] Run On Node [%s] Once Execute Failed, Output: %s, Error: %s", task.ID, task.RunOn, result, runErr.Error()),
+			To: notifiedUsers,
+			OccurTime: time.Now().Format(utils.TimeFormatSecond),
+		}
+
+		go notify.Send(msg)
+	} else {
+		// Task Success -> Record Log
+		err = task.Success(taskLogId, t, result, 0)
+		if err != nil {
+			logger.GetLogger().Warn(fmt.Sprintf("Failed To Write Task Log: %v\n", err))
+		}
+	}
+}
+
+// @Description: Create Task Cron Callback Func
+func CreateTaskCallback(task *Task) cron.FuncJob {
+	h := NewHandler(task)
+	if h == nil {
+		logger.GetLogger().Error("Failed To Create Task Handler")
+		return nil
+	}
+
+	taskFunc := func() {
+		logger.GetLogger().Info(fmt.Sprintf("Start The Task [%s] Command [%s]", task.Name, task.Command))
+		var execTimes int = 1
+		if task.RetryTimes > 0 {
+			execTimes += task.RetryTimes
+		}
+
+		var tryTimes int = 0
+		var output string 
+		var runErr error // Task Run Error
+		var err error    // Func Run Error
+		var taskLogId int
+		t := time.Now()
+		taskLogId, err = task.CreateTaskLog()
+		if err != nil {
+			logger.GetLogger().Warn(fmt.Sprintf("Failed To Write Task Log: %v\n", err))
+		}
+
+		for tryTimes < execTimes {
+			output, runErr = h.Run(task)
+			if runErr == nil {
+				// Task Success
+				err = task.Success(taskLogId, t, output, tryTimes)
+				if err != nil {
+					logger.GetLogger().Warn(fmt.Sprintf("Failed To Write Task Log: %v", err))
+				}
+				return 
+			}
+
+			tryTimes ++
+			if tryTimes < execTimes {
+				logger.GetLogger().Warn(fmt.Sprintf("Task Execution Failed: %v", runErr))
+				if task.RetryInterval > 0 {
+					time.Sleep(time.Duration(task.RetryInterval) * time.Second)
+				} else {
+					time.Sleep(time.Duration(tryTimes) * time.Minute)
+				}
+			}
+		}
+
+		// Task Failed
+		err = task.Fail(taskLogId, t, runErr.Error(), execTimes-1)
+		if err != nil {
+			logger.GetLogger().Warn(fmt.Sprintf("Failed To Write Task Log: %v", err))
+		}
+		node := &model.Node{UUID: task.RunOn}
+		err = node.FindByUUID()
+		if err != nil {
+			logger.GetLogger().Warn(fmt.Sprintf("Failed to find node: %v\n", err))
+		}
+
+		var notifiedUsers []string
+		for _, userId := range task.NotifyToArray {
+			userModel := &model.User{
+				ID: userId,
+			}
+			err = userModel.FindById()
+			if err != nil {
+				logger.GetLogger().Warn(fmt.Sprintf("Failed to Find User: %d\n", userId))
+				continue
+			}
+
+			notifiedUsers = append(notifiedUsers, userModel.Email)
+		}
+
+		msg := &notify.Message{
+			Type: task.NotifyType,
+			IP: fmt.Sprintf("%s:%s", node.IP, node.PID),
+			Subject: fmt.Sprintf("任务 [%s] 执行失败", task.Name),
+			Body: fmt.Sprintf("Task [%d] Run On Node [%s] Once Execute Failed, Output: %s, Error: %s", task.ID, task.RunOn, output, runErr.Error()),
+			To: notifiedUsers,
+			OccurTime: time.Now().Format(utils.TimeFormatSecond),
+		}
+
+		go notify.Send(msg)
+
+	}
+
+	return taskFunc
 }
 
 
